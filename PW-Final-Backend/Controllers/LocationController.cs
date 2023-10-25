74058ba4b97ca9d25a0ecd5983037892355e26c6
brendaahumada <bahumada@mail.austral.edu.ar>
2023-10-25 17:02:33 -0300

    add controller

---
+++ PW-Final-Backend/Controllers/LocationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PW_Final_Backend.Controllers
+{
+    public class LocationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
